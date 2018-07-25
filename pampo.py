da4a0a2bde3e379db289e4c27776cdfad19ecca7
arian <arian@arian.thinkpad>
2018-07-25 14:33:10 +0100

    initial import

---
+++ pampo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import click
+from .ner import *
+
+@click.command()
+@click.argument('input', type=click.File('rb'))
+def main(input):
+    """Console script for Pampo. Inform an input file and it returns detected entities."""
+
+    content = ''
+    while True:
+        chunk = input.read(1024)
+        if not chunk:
+            break
+        content += chunk
+
+    entities = extract_entities(content)
+    for entity in entities:
+        print(entity)
+
+if __name__ == "__main__":
+    main()
